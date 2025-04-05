4a234bc89abd7538ceeab1adb059c1bcca72451d
Jaos <andresorozco1098@hotmail.com>
2025-04-05 13:30:34 -0500

    Small changes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center ">
+      <div className="bg-blue-500 h-2/4 w-1/4">01</div>
+    {/* <div className="grid grid-cols-1 place-content-center bg-blue-500 h-2/4 mt-40">
+
+    </div> */}
+    </div>
+  );
+}
